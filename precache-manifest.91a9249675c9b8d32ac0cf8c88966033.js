dbd5085b2cc3fba2e85ca8ecc01a731d2ebbcf39
pavlenstory <55143655+pavlenstory@users.noreply.github.com>
2020-07-30 15:08:52 +0300

    Updates

---
+++ precache-manifest.91a9249675c9b8d32ac0cf8c88966033.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e52cad8c88b568b16ec835fb9049d1b",
+    "url": "/NASA/index.html"
+  },
+  {
+    "revision": "e801ec617143a44ca25b",
+    "url": "/NASA/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "cc32d509322b47b01209",
+    "url": "/NASA/static/css/main.0ca986f8.chunk.css"
+  },
+  {
+    "revision": "e801ec617143a44ca25b",
+    "url": "/NASA/static/js/2.fe9866c7.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/NASA/static/js/2.fe9866c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc32d509322b47b01209",
+    "url": "/NASA/static/js/main.0416be14.chunk.js"
+  },
+  {
+    "revision": "5a80dde51c0e6f3ad0bf",
+    "url": "/NASA/static/js/runtime-main.3a7f357c.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/NASA/precache-manifest.91a9249675c9b8d32ac0cf8c88966033.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/NASA/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
